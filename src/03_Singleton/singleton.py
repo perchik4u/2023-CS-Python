c6bc1a34abafb27034505720f725bbd919af4df7
perchik4u <rangemihred2@gmail.com>
2023-12-14 11:03:49 +1000

    Add Singleton solve

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,2 +1,8 @@
 class Singleton:
-    pass
+    def __new__(cls):
+        if not hasattr(cls, 'single_cls'):
+            cls.single_cls = super(Singleton, cls).__new__(cls)
+        return cls.single_cls
+    
+
+a = Singleton()

cf6d7c425a4172adf3ec962518623003f0566522
perchik4u <rangemihred2@gmail.com>
2023-12-21 12:32:52 +1000

    fix
    fix
    
    fix

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,8 +1,7 @@
 class Singleton:
     def __new__(cls):
-        if not hasattr(cls, 'single_cls'):
+        if not hasattr(cls, "single_cls"):
             cls.single_cls = super(Singleton, cls).__new__(cls)
         return cls.single_cls
-    
 
 a = Singleton()

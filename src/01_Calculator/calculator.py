75a8a85f6d3ee012b8e696f6ecf2a390fdb10aaa
perchik4u <rangemihred2@gmail.com>
2023-12-21 12:12:18 +1000

    111

--- src/01_Calculator/calculator.py
+++ src/01_Calculator/calculator.py
@@ -1,56 +1,55 @@
 from operator import add, mul, sub, truediv
-from typing import List, Optional, Union
+from typing import List
 
 ops = {"+": add, "-": sub, "*": mul, "/": truediv}
 
 
-def _split_if_string(string_or_list: Union[List[str], str]) -> List[str]:
-    return string_or_list.split() if isinstance(string_or_list, str) else string_or_list
-
-
-def prefix_evaluate(prefix_equation: Union[List[str], str]) -> Optional[int]:
-    if not prefix_equation:
+def prefix_evaluate(prefix_evaluation):
+    if not prefix_evaluation:
         return None
-    prefix_equation = _split_if_string(prefix_equation)
-    value_stack = []
-    while prefix_equation:
-        el = prefix_equation.pop()
-        if el not in ops:
-            value_stack.append(int(el))
-        else:
-            r_val = value_stack.pop()
-            l_val = value_stack.pop()
-            operation = ops[el]
-            value_stack.append(operation(r_val, l_val))
 
-    return value_stack[0]
+    stack = []
+    prefix_evaluation = prefix_evaluation.split() \
+        if isinstance(prefix_evaluation, str) \
+        else prefix_evaluation
+
+    for token in reversed(prefix_evaluation):
+        if token.isdigit():
+            stack.append(int(token))
+        elif token in ops:
+            operand1 = stack.pop()
+            operand2 = stack.pop()
+
+            result = ops[token](operand1, operand2)
+            stack.append(result)
+
+    return stack[0]
 
 
-def to_prefix(equation: str) -> str:
-    op_stack = []
-    prefix = []
+def to_prefix(equation: str) -> List[str]:
+    precedence = {"+": 1, "-": 1, "*": 2, "/": 2}
+    operators = set("+*-/")
+    output = []
+    stack: List[object] = []
 
-    for el in equation.split()[::-1]:
-        if el.isdigit():
-            prefix.append(el)
-        elif el == ")" or el in ops:
-            op_stack.append(el)
-        elif el == "(":
-            while op_stack and op_stack[-1] != ")":
-                prefix.append(op_stack.pop())
-            op_stack.pop()
+    for token in reversed(equation.split()):
+        if token.isdigit():
+            output.append(token)
+        elif token in operators:
+            while stack and stack[-1] != ")" and precedence[token] <= precedence.get(stack[-1], 0):
+                output.append(stack.pop())
+            stack.append(token)
+        elif token == ")":
+            stack.append(token)
+        elif token == "(":
+            while stack and stack[-1] != ")":
+                output.append(stack.pop())
+            stack.pop()
 
-            if ")" not in op_stack:
-                while op_stack:
-                    if op_stack[-1] == ")":
-                        op_stack.pop()
-                    else:
-                        prefix.append(op_stack.pop())
+    while stack:
+        output.append(stack.pop())
 
-    while op_stack:
-        prefix.append(op_stack.pop())
-
-    return " ".join(prefix[::-1])
+    return list(reversed(output))
 
 
 def calculate(equation: str) -> int:
